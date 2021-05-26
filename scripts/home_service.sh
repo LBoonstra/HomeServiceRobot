5c044bec0ac791853312e6fe21de45fcd77e134b
LBoonstra <lisa_boonstra@hotmail.com>
2021-05-26 23:55:13 +0200

    update add_markers
    moved rviz launch files to add_markers package
    updated home_service.sh

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -2,10 +2,10 @@
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/4_rooms.world" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml" &
+#sleep 5
+#xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"  &
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"  &
-sleep 5
-xterm -e "roslaunch rvizConfig rviz_config.launch" &
+xterm -e "roslaunch add_markers rviz_config.launch" &
 sleep 5
 xterm -e "rosrun pick_objects pick_objects" &
 sleep 5
