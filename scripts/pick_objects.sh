26e94908d453da6e5fc840e95ebff25b84ee4295
LBoonstra <lisa_boonstra@hotmail.com>
2021-05-27 18:12:31 +0200

    Update scripts and tmove map files
    - Adjusted scripts
    - Moved world files to turtlebot_gazebo/worlds
    - Moved map files to turtlebot_gazebo/maps

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/4_rooms.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/4_rooms.world" &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find maps)/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find turtlebot_gazebo)/map.yaml" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"  &
 sleep 5
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/4_rooms.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/4_rooms.world" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5 
