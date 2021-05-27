56a79fbf587ee9962e71eb6bc77e7c0e168ae795
LBoonstra <lisa_boonstra@hotmail.com>
2021-05-27 14:22:05 +0200

    update maps
    * Created package for map files
    * Removed map folder
    * adjusted scripts

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/4_rooms.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/4_rooms.world" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5 
